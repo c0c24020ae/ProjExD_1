db88e1b56fda7af8686f335d778a28541c0eae01
c0c24020ae <c0c24020ae@edu.teu.ac.jp>
2025-04-15 16:03:43 +0900

    練習８

--- pygame_image.py
+++ pygame_image.py
@@ -12,6 +12,7 @@ def main():
     bg_img = pg.image.load("fig/pg_bg.jpg")
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img,True,False)
+    bg_img2 = pg.transform.flip(bg_img,True,False)
     tmr = 0
     while True:
         for event in pg.event.get():
@@ -21,7 +22,7 @@ def main():
         x = tmr
         screen.blit(bg_img, [-x, 0])
         screen.blit(kk_img, [-x+1600, 0])
-        #screen.blit(kk_img, [300, 200])
+        screen.blit(kk_img, [300, 200])
         pg.display.update()
         tmr += 1        
         clock.tick(200)
